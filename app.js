6321d96582cfd0574598dad0fabac9427febfb05
johirjohnny <johirjohnny96@gmail.com>
2021-02-13 00:20:12 +0600

    adding button

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const displayCountries = countries => {                    //a function with cou
     const countriesDiv = document.getElementById('countries');  //countriesDiv with inside in countris ID as like ul
         countries.forEach(country => {
         const countryDiv = document.createElement('div');               //countryDiv or li create
-        countriesDiv.className = 'country';            
+        countryDiv.className = 'country';            
         const countryInfo = `
         <h3 class="country-name" > ${country.name}</h3>
         <p> ${country.capital}</p>
+        <button onclick = "displayCountryDetail('${country.name}')">show detail </button>
         `;
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
     });
+}
 //     for (let index = 0; index < countries.length; index++) {
 //         const country = countries[index];
 //         //console.log(country.name);
@@ -36,4 +38,19 @@ const displayCountries = countries => {                    //a function with cou
 //         countryDiv.innerHTML = countryInfo;
 //         countriesDiv.appendChild(countryDiv);
 //     }
+const displayCountryDetail = name =>{
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch (url)
+    .then(res => res.json())
+    .then(data => renderCountryInfo(data[0]))
+}
+const renderCountryInfo = country =>{
+    //console.log(country);
+    const countryDiv = document.getElementById('countryDetail');
+    countryDiv.innerHTML= `
+    <h1>${country.name}</h1>
+    <p>Population : ${country.population}</p>
+    <p> Area : ${country.area}</p>
+    <img src ="${country.flag}"></img>`
+    
 }
